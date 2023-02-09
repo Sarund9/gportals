8f9b5630d44fbcfa4b6b875304b8ea85608476e0
Sarund9 <sebi.obrutsky@gmail.com>
2023-02-09 15:38:11 -0300

    Project Setup

---
+++ gportals/register_types.cpp
@@ -0,0 +1,13 @@
+#include "register_types.h"
+// #include "core/class_db.h"
+// #include "calculatorRef.h"
+// #include "calculatorNode.h"
+
+void register_gportals_types(){
+	// ClassDB::register_class<CalculatorNode>();
+	// ClassDB::register_class<CalculatorRef>();
+	
+}
+
+void unregister_gportals_types() {
+}
---
+++ gportals/register_types.h
@@ -0,0 +1,9 @@
+void register_gportals_types();
+void unregister_gportals_types();
+
+/*
+	void register_MODULNAME_types();
+	void unregister_MODULNAME_types();
+
+	MODULNAME ->> gportals
+*/
